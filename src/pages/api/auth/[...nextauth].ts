ef6c54b6e0888b83a7ab8196a42aec9e36b585ad
João Victo Dantas <67969013+Arsene1999@users.noreply.github.com>
2021-09-09 10:00:23 -0300

    fauna funcionando

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import {query as qFauna} from 'faunadb';
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-    
+ 
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
@@ -11,4 +13,23 @@ export default NextAuth({
     }),
     
   ],
+  callbacks:{
+    async signIn(user, account, profile){
+      const { email } = user;
+      
+      try{
+        await fauna.query(
+          qFauna.Create(
+            qFauna.Collection('users'),
+            { data: { email } }
+          )
+        );
+        return true;
+      }catch(err){
+        console.log(err);
+        return false;
+      }
+      
+    }
+  },
 })
